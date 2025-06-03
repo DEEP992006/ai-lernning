5c12818c457c5e659031f7497f96cda94966737e
DEEP992006 <deepsurati02@gmail.com>
2025-06-03 06:35:11 +0000

    feat: complete navbar UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,8 @@
-import { NavLink } from "react-router"
-function App() {
-  return (
-    <nav>
-      <NavLink to="/" className="" end>
-        Home
-      </NavLink>
-      <NavLink to="/trending" className="active:text-green-500">
-        Trending Concerts
-      </NavLink>
-      <NavLink to="/concerts">All Concerts</NavLink>
-      <NavLink to="/account">Account</NavLink>
-      
-    </nav>
+import React from 'react'
 
+const App = () => {
+  return (
+    <div className=''>App</div>
   )
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from "react-router";
+import Practice from './new/sort.tsx';
+import Navbar from './layout/Navbar.tsx';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
- <BrowserRouter>
-     <Routes>
-      <Route path="/" element={<App />} />
-      
-    </Routes>
+    <BrowserRouter>
+     <div className='flex min-h-screen w-full flex-col'>
+      <div>
+        <Navbar />
+        </div>
+        <div className='flex-1'>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/p" element={<Practice/>} />
+            {/* Add other routes here */}
+          </Routes>
+          </div>
+      </div>
+   
     </BrowserRouter>
   </StrictMode>
 )
