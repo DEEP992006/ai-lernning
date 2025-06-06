a06d0e1a398342f400dd21a85be3861ec4c85bde
Deep Surati <deepsurati02@gmail.com>
2025-06-06 12:51:46 +0530

    feat(course): complete course view and exception handling  logic in backend

--- backend/features/course/course_controller.js
+++ backend/features/course/course_controller.js
@@ -1,7 +1,6 @@
 //  Controller for New Course
 import express from "express";
-import { createNewCourse } from "./course_service.js";
-
+import { createNewCourse, getcourse } from "./course_service.js";
 export const newCourseController = async (req, res) => {
   const {
     name,
@@ -11,15 +10,36 @@ export const newCourseController = async (req, res) => {
     category,
     token
   } = req.body;
-
-  const newCourse = await createNewCourse(
-    name,
-    description,
-    difficulty,
-    chapter,
-    category,
-    token
-  );
-
-  res.json({ new: newCourse });
+  //check if any firld missing
+if(!name|!description|!difficulty|!chapter|!category|!token){
+    res.status(400).json({"message":"all field are required"})
+}
+//send data to sevice file
+try {
+    const newCourse = await createNewCourse(
+      name,
+      description,
+      difficulty,
+      chapter,
+      category,
+      token
+    );
+  //retuening response
+    res.json({ new: newCourse });
+} catch (error) {
+    //if any error send this
+    res.status(400).json({"message":error.message})
+}
 };
+export const viewcoursecontroller = async (req,res) => {
+    //get cou
+    const {id,token} = req.body
+    // console.log(uid);
+    try {
+      const course = await getcourse(id,token)
+      res.status(200).json({"course":course})
+    } catch (error) {
+      res.status(400).json({"error":error.message})
+    }
+    
+}
--- backend/features/course/course_route.js
+++ backend/features/course/course_route.js
@@ -1,10 +1,10 @@
 // Express Setup and Course Route
 import express from "express";
-import { newCourseController } from "./course_controller.js";
+import { newCourseController, viewcoursecontroller } from "./course_controller.js";
 
 const router = express.Router();
 
 // Create New Course Endpoint
 router.post("/new", newCourseController);
-
+router.post("/",viewcoursecontroller)
 export default router;
